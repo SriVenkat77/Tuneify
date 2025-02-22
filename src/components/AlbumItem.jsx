38c2e039753b33d28a6b486c70026c1db471db1d
SriVenkat77 <venkateshwaran1112@gmail.com>
2025-02-23 01:22:39 +0530

    Updated

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -7,7 +7,7 @@ const AlbumItem = ({image,name,desc,id}) => {
 
   return (
     <div onClick={()=>navigate(`/album/${id}`)} className='min-w-[150px] sm:min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-red-700'>
-      <img className='w-full min-h-[120px] sm:min-h-[180px] rounded object-cover' src={image} alt="" />
+      <img className='w-full min-h-[120px] sm:min-h-[180px]  max-h-[125px] rounded object-cover' src={image} alt="" />
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
