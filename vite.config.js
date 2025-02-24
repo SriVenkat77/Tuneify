b5914f09e6156f271dddab8bf6d1950255ce494c
SriVenkat77 <venkateshwaran1112@gmail.com>
2025-02-25 00:17:09 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this if deploying to a subpath (e.g., '/Tuneify/')
+  base: './',  // <-- Add this to fix Netlify issues
 })
